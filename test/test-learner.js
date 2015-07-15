09e88c0141134da11c7c58d75c3da84f9ed0efcb
Marc Portier <marc.portier@gmail.com>
2015-07-15 13:15:06 +0200

    suggested extra tests (pending)

---
+++ test/test-learner.js
@@ -0,0 +1,18 @@
+/*jslint node: true */
+/*jslint nomen: true */
+/*global describe*/
+/*global it*/
+/*global before*/
+
+"use strict";
+
+var assert    = require('chai').assert,
+    SchemaDoc = require('../lib/shmdoc').SchemaDoc;
+
+describe('data learning', function () {
+
+    it('should be able to read json');
+    it('should be able to read csv');
+    it('should be able to read xml');
+
+});
---
+++ test/test-loadbase.js
@@ -0,0 +1,18 @@
+/*jslint node: true */
+/*jslint nomen: true */
+/*global describe*/
+/*global it*/
+/*global before*/
+
+"use strict";
+
+var assert    = require('chai').assert,
+    SchemaDoc = require('../lib/shmdoc').SchemaDoc;
+
+describe('base loader', function () {
+
+    it('should load from json');
+    it('should load from csv');
+    it('should load remote ');
+
+});
