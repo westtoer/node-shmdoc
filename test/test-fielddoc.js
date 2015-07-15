21bb9445a9db03d746706d28cb59bdf80eae4ea1
Marc Portier <marc.portier@gmail.com>
2015-07-15 15:12:38 +0200

    new tests show problem with min-max logic

--- test/test-fielddoc.js
+++ test/test-fielddoc.js
@@ -81,12 +81,74 @@ describe('value analyzer', function () {
         });
     });
 
-    it('should count use and empty correctly');
+    it('should count use and empty correctly', function () {
+        var sd = new SchemaDoc(), tobj = [{x: 1, y: 1}, {x: 1, y: ""}, {x: 1} ],
+            baseKeys;
 
-    it('should determine min-max correctly');
+        sd.traverseValue([], tobj);
+        assert.isNotNull(sd.base,
+                         "internal base should exist");
+        baseKeys = Object.keys(sd.base);
+        assert.equal(baseKeys.length, 2,
+                     "number of keys don't match base=" + baseKeys + " - expect = 2");
+        assert.deepEqual(baseKeys.sort(), ["*.x", "*.y"], "keys don't match ['*.x,''*.y']");
 
-    it('should determine min-max-length correctly');
+        assert.deepEqual(
+            sd.base["*.x"].count,
+            {used: 3, empty: 0, bytype: {integer: 3}},
+            "unexpected usage/empty count for *.x"
+        );
+        assert.deepEqual(
+            sd.base["*.y"].count,
+            {used: 2, empty: 1, bytype: {integer: 1}},
+            "unexpected usage/empty count for *.y"
+        );
+    });
 
-    it('should check references correctly');
+    it('should determine min-max correctly', function () {
+        var sd = new SchemaDoc(), tobj = [{x: 1, y: 10}, {x: 2, y: 20}, {x: 3} ],
+            baseKeys;
+
+        sd.traverseValue([], tobj);
+        assert.isNotNull(sd.base,
+                         "internal base should exist");
+        baseKeys = Object.keys(sd.base);
+
+        assert.ok(sd.base["*.x"].learnable, "learnable mode for *.x");
+        assert.equal(sd.base["*.x"].min, 1, "min for *.x");
+        assert.equal(sd.base["*.x"].max, 3, "max for *.x");
+
+        assert.ok(sd.base["*.y"].learnable, "learnable mode for *.y");
+        assert.equal(sd.base["*.y"].min, 10, "min for *.y");
+        assert.equal(sd.base["*.y"].max, 20, "max for *.y");
+
+    });
+
+    it('should determine min-max-length correctly', function () {
+        var sd = new SchemaDoc(), tobj = [{x: "xxx", y: "yyyyy"}, {x: "xx", y: "yy"}, {x: "x"} ],
+            baseKeys;
+
+        sd.traverseValue([], tobj);
+        assert.isNotNull(sd.base,
+                         "internal base should exist");
+        baseKeys = Object.keys(sd.base);
+
+        assert.ok(sd.base["*.x"].learnable, "learnable mode for *.x");
+        assert.equal(sd.base["*.x"].minlen, 1, "min for *.x");
+        assert.equal(sd.base["*.x"].maxlen, 3, "max for *.x");
+
+        assert.ok(sd.base["*.y"].learnable, "learnable mode for *.y");
+        assert.equal(sd.base["*.y"].minlen, 2, "min for *.y");
+        assert.equal(sd.base["*.y"].maxlen, 5, "max for *.y");
+
+    });
+
+    it('should report errors of new-values correctly');
+
+    it('should report errors of boundaries correctly');
+
+    it('should report errors of type correctly');
+
+    it('should report errors of references correctly');
 
 });
