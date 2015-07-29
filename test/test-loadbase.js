83af0a022d1d8b97535a2c49d7d6fb4b894fec7f
Marc Portier <marc.portier@gmail.com>
2015-07-29 13:04:27 +0200

    more test harnass

--- test/test-loadbase.js
+++ test/test-loadbase.js
@@ -7,12 +7,50 @@
 "use strict";
 
 var assert    = require('chai').assert,
+    path      = require('path'),
     SchemaDoc = require('../lib/shmdoc').SchemaDoc;
 
 describe('base loader', function () {
 
-    it('should load from json');
-    it('should load from csv');
-    it('should load remote ');
+    function loadAndTest(ext, done) {
+        var sd = new SchemaDoc(), bpath = path.join(__dirname, "shmdoc-base." + ext);
+
+        sd.loadBase(bpath, function () {
+            assert.ok(sd.base, "base should be loaded");
+            assert.ok(sd.base.testkey, "base should have at least this key");
+            // remove the unknown/empty elements
+            delete sd.base.testkey.created;
+            delete sd.base.testkey.format;
+            delete sd.base.testkey.isReferenceToKey;
+            delete sd.base.testkey.isReferenceable;
+            delete sd.base.testkey.learnable;
+            delete sd.base.testkey.list;
+            delete sd.base.testkey.updated;
+
+            //test the remainder
+            assert.deepEqual(
+                sd.base,
+                {
+                    testkey: {
+                        key: 'testkey',
+                        type: 'text',
+                        min: 'a',
+                        max: 'z',
+                        minlen: '1',
+                        maxlen: '100'
+                    }
+                }
+            );
+            done();
+        });
+    }
+
+    it('should load from json', function (done) {
+        loadAndTest("json", done);
+    });
+
+    it('should load from csv', function (done) {
+        loadAndTest("csv", done);
+    });
 
 });
